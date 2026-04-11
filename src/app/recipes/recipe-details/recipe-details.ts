87f2b225d8dea0a079d4e49c69f7a660882ea262
abdallahkhaliid <abdallahsh404@gmail.com>
2026-04-12 01:38:40 +0200

    chore: initial project structure

---
+++ src/app/recipes/recipe-details/recipe-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details',
+  imports: [],
+  templateUrl: './recipe-details.html',
+  styleUrl: './recipe-details.scss',
+})
+export class RecipeDetails {
+
+}
